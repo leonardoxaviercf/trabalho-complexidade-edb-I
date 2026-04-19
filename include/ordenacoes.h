621d9b15e9bc3af839bf2015dcb715312c965b35
leonardoxaviercf <leonardocruzfilho8@gmail.com>
2026-04-19 09:39:21 -0300

    feat: cabeçalho das ordenações

---
+++ include/ordenacoes.h
@@ -0,0 +1,10 @@
+#ifndef ORDENACOES_H
+#define ORDENACOES_H
+using namespace std;
+
+#include <vector>
+
+void selectionSort(vector<int>& vet);
+void mergeSort(vector<int>& vet, int esquerda, int direita);
+
+#endif
