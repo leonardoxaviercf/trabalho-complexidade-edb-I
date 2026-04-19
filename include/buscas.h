0805356c0ea4f032e6a23648766b9712f627896c
leonardoxaviercf <leonardocruzfilho8@gmail.com>
2026-04-19 09:10:34 -0300

    feat: cabeçalho das buscas

---
+++ include/buscas.h
@@ -0,0 +1,10 @@
+#ifndef BUSCAS_H
+#define BUSCAS_H
+using namespace std;
+
+#include <vector>
+
+int buscaSequencial(const vector<int>& vet, int alvo);
+int buscaBinaria(const vector<int>& vet, int alvo);
+
+#endif
