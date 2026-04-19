596cafc9ecb38781b0ce26ca431595adb8b3f71d
leonardoxaviercf <leonardocruzfilho8@gmail.com>
2026-04-19 11:21:53 -0300

    feat: cabeçalho dos geradores de vetor

---
+++ include/gerador_dados.h
@@ -0,0 +1,10 @@
+#ifndef GERADOR_DADOS_H
+#define GERADOR_DADOS_H
+using namespace std;
+
+#include <vector>
+
+vector<int> gerarVetorOrdenado(int tamanho);
+vector<int> gerarVetorInverso(int tamanho);
+
+#endif
